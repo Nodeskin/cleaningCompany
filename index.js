e244d524ad0ad10aacd02e1897a5e951698b4b5d
nodeSkin <shittujamiu@gmail.com>
2023-05-13 21:59:56 +0100

    fixed hamburger and mediaquery

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const menu = document.querySelector('.menu-icon')
-const close = document.querySelector('.close-icon')
-const menuNav = document.querySelector('.menu-navbar')
-menu.addEventListener('click', () =>{
-menu.style.display = "none";
-close.style.display = "flex"
-menuNav.style.display = "flex"
+const menu = document.querySelector(".menu-icon");
+const close = document.querySelector(".close-icon");
+const menuNav = document.querySelector(".one");
+const menuDropDown = document.querySelector(".menu-right-nav-bar")
 
-})
+menu.addEventListener("click", () => {
+  menu.style.display = "none";
+  close.style.display = "flex";
+  menuDropDown.style.display = "flex";
+});
 
-close.addEventListener('click', () =>{
-    menu.style.display = "flex";
-    close.style.display = "none"
-    menuNav.style.display = "none"
-        
-    })
-
-
-
+close.addEventListener("click", () => {
+  menu.style.display = "flex";
+  close.style.display = "none";
+  menuDropDown.style.display = "none";
+});
